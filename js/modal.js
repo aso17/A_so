a870a374c2a4b57f1e1d7a4847033632f5943bb9
aso17 <bagussajiwo305@gmail.com>
2022-01-21 19:48:52 +0700

    update17

--- js/modal.js
+++ js/modal.js
@@ -1,55 +1,162 @@
-const db = [
+const dbMayora = [
   {
-    mayora: `Aplikasi ini diperuntukan untuk melakukan pengendalian terhadap karyawan dan visitor Pada PT.Mayora TBK yang memang melakukan tindakan yang tidak sesuai Standar operasional prosedur.
-  Aplikasi dibuat menggunakan Framwork Php CodeIgniter versi 3.0,Bootstrup versi 4.0, Dengan DBMS MySql.
-  `,
+    judul: "Aplikasi Laporan Pelanggaran",
   },
   {
-    evoting: `Aplikasi ini gunakan Untuk Voting kepala Desa, Rt, Rw, yang dilakukan secara online. Aplikasi dibuat menggunakan Framwork Php CodeIgniter versi 3.0,Bootstrup versi 4.0, Css, dan JavaScript Dengan DBMS MySql.`,
+    spesifikasi: ["PHP Language", "FrameWork Codeigniter Version 3.0", "Bootstrap Version 4.0", "Css", "DataTables", "jquery", "SBAdmin", "Fontawesome", "DBMS MySql"],
   },
   {
-    quila: `Aplikasi ini gunakan Untuk melakukan Transaksi Penjualan pada toko Sepatu Selain itu juga bisa Melihat data pelanggan serta melakukan laporan harian Apliksi dibuat Framwork Php CodeIgniter versi 3.0,Bootstrup versi 4.0, Css, dan JavaScript Dengan DBMS MySql `,
-  },
-  {
-    adis: `Aplikasi ini gunakan Untuk melakukan Monitoring Planing pembuatan Sample Sepatu Pada PT.Adis Dimension Footwear, Selain itu juga dapat melihat bahan baku yang tersedia saat ini sehingga mempermudah user dalam melakukan planing . Aplikasi dibuat menggunakan Framwork Php CodeIgniter versi 3.0,Bootstrup versi 4.0, Css, dan JavaScript Dengan DBMS MySql`,
-  },
-  {
-    spk: `Aplikasi ini gunakan Untuk Pengambilan keputusan dalam menentukan Sanksi kinerja karyawan yang sifatnya terstruktur dengan beberapa kategori pelanggaran. Aplikasi dibuat menggunakan Framwork Php Laravel versi 7.0,Bootstrup versi 5.0, Css, dan JavaScript Dengan DBMS MySql `,
+    description: "Aplikasi ini diperuntukan untuk melakukan pengendalian terhadap karyawan dan visitor Pada PT.Mayora TBK yang memang melakukan tindakan yang tidak sesuai Standar operasional prosedur",
   },
 ];
-const main = document.querySelector(".main");
-const detail = document.querySelectorAll(".detail");
-const wraper = document.querySelector(".modal-wrapper");
-const boxWrapper = document.querySelector(".box-wrapper");
+const dbSpk = [
+  {
+    judul: "Aplikasi Decision Support System",
+  },
+  {
+    spesifikasi: ["PHP Language", "FrameWork Laravel Version 7.1", "Bootstrap Version 5.0", "Css", "DataTables", "jquery", "Vanila JavaScrip", "Adminlte", "Fontawesome", "DBMS MySql"],
+  },
+  {
+    description: "Aplikasi ini gunakan Untuk Pengambilan keputusan dalam menentukan Sanksi kinerja karyawan yang sifatnya terstruktur dengan beberapa kategori pelanggaran dengan menggunakan metode Simple Additive Weighting (SAW)",
+  },
+];
+const dbEvoting = [
+  {
+    judul: "Aplikasi Evoting(Pemilihan)",
+  },
+  {
+    spesifikasi: ["PHP Language", "FrameWork Codeigniter Version 3.0", "Bootstrap Version 4.0", "Css", "DataTables", "jquery", "Vanila JavaScrip", "Adminlte", "Fontawesome", "DBMS MySql"],
+  },
+  {
+    description: "Aplikasi ini gunakan Untuk Voting kepala Desa, Rt, Rw, yang dilakukan secara online",
+  },
+];
+const dbQuila = [
+  {
+    judul: "Aplikasi Penjulan",
+  },
+  {
+    spesifikasi: ["PHP Language", "FrameWork Codeigniter Version 3.0", "Bootstrap Version 4.0", "Css", "DataTables", "jquery", "Vanila JavaScrip", "Adminlte", "Fontawesome", "DBMS MySql"],
+  },
+  {
+    description: "Aplikasi ini gunakan Untuk melakukan Transaksi Penjualan pada toko Sepatu Selain itu juga bisa Melihat data pelanggan serta melakukan laporan harian",
+  },
+];
+const dbPcc = [
+  {
+    judul: "Aplikasi Monitoring Planing",
+  },
+  {
+    spesifikasi: ["PHP Language", "FrameWork Codeigniter Version 3.0", "Bootstrap Version 4.0", "Css", "DataTables", "jquery", "Vanila JavaScrip", "Adminlte", "Fontawesome", "DBMS MySql"],
+  },
+  {
+    description:
+      "Aplikasi ini gunakan Untuk melakukan Monitoring Planing pembuatan Sample Sepatu Pada PT.Adis Dimension Footwear, Selain itu juga dapat melihat bahan baku yang tersedia saat ini sehingga mempermudah user dalam melakukan planing",
+  },
+];
+
+const detail = document.querySelectorAll("#detail");
 detail.forEach(function (b) {
-  b.addEventListener("click", function () {
-    wraper.classList.toggle("open");
-    const x = parseInt(b.getAttribute("data-detail"));
-    isi(x);
+  b.addEventListener("click", () => {
+    let n = b.getAttribute("data-detail");
+    isi(n);
   });
 });
 
-const dataBtn = document.querySelector(".detail");
+const parent = document.querySelector(".modal-body");
+const title = document.querySelector(".judul");
+const spec = document.querySelector("#spec");
+const close = document.querySelector("#tutup");
+const isiSpec = document.querySelector(".isiSpec");
+const desc = document.querySelector(".desc");
+const PDesc = document.querySelector(".PaDesc");
+const ptitle = document.querySelector(".modal-header");
+const childSpc = document.querySelector("#childSpc");
+
 function isi(x) {
   if (x == 0) {
-    let textmayora = db[x].mayora;
-    main.textContent = textmayora;
+    let texttitle = dbMayora[x].judul;
+    let specifikasi = dbMayora[1].spesifikasi;
+    title.textContent = texttitle;
+    for (let i = 0; i < specifikasi.length; i++) {
+      let li = document.createElement("li");
+      let datali = `${specifikasi[i]}`;
+      li.append(datali);
+      spec.appendChild(li);
+      close.addEventListener("click", () => {
+        li.parentNode.removeChild(li);
+        location.reload();
+      });
+    }
+    let newDesc = `<p class="text-light"> ${dbMayora[2].description}</p>`;
+    desc.insertAdjacentHTML("beforebegin", newDesc);
   } else if (x == 1) {
-    let textEvoting = db[x].evoting;
-    main.textContent = textEvoting;
+    let texttitle = dbSpk[x - 1].judul;
+    let specifikasi = dbSpk[1].spesifikasi;
+    title.textContent = texttitle;
+    for (let i = 0; i < specifikasi.length; i++) {
+      let li = document.createElement("li");
+      let datali = `${specifikasi[i]}`;
+      li.append(datali);
+      spec.appendChild(li);
+      close.addEventListener("click", () => {
+        li.parentNode.removeChild(li);
+        location.reload();
+      });
+    }
+    //description
+    let newDesc = `<p class="text-light"> ${dbSpk[2].description}</p>`;
+    desc.insertAdjacentHTML("beforebegin", newDesc);
   } else if (x == 2) {
-    let textQuila = db[x].quila;
-    main.textContent = textQuila;
+    let texttitle = dbEvoting[x - 2].judul;
+    let specifikasi = dbEvoting[1].spesifikasi;
+    title.textContent = texttitle;
+    for (let i = 0; i < specifikasi.length; i++) {
+      let li = document.createElement("li");
+      let datali = `${specifikasi[i]}`;
+      li.append(datali);
+      spec.appendChild(li);
+      close.addEventListener("click", () => {
+        li.parentNode.removeChild(li);
+        location.reload();
+      });
+    }
+
+    let newDesc = `<p class="text-light"> ${dbEvoting[2].description}</p>`;
+    desc.insertAdjacentHTML("beforebegin", newDesc);
   } else if (x == 3) {
-    let textadis = db[x].adis;
-    main.textContent = textadis;
+    let texttitle = dbQuila[x - 3].judul;
+    let specifikasi = dbQuila[1].spesifikasi;
+    title.textContent = texttitle;
+    for (let i = 0; i < specifikasi.length; i++) {
+      let li = document.createElement("li");
+      let datali = `${specifikasi[i]}`;
+      li.append(datali);
+      spec.appendChild(li);
+      close.addEventListener("click", () => {
+        li.parentNode.removeChild(li);
+        location.reload();
+      });
+    }
+
+    let newDesc = `<p class="text-light"> ${dbQuila[2].description}</p>`;
+    desc.insertAdjacentHTML("beforebegin", newDesc);
   } else {
-    let textspk = db[x].spk;
-    main.textContent = textspk;
+    let texttitle = dbPcc[x - 4].judul;
+    let specifikasi = dbPcc[1].spesifikasi;
+    title.textContent = texttitle;
+    for (let i = 0; i < specifikasi.length; i++) {
+      let li = document.createElement("li");
+      let datali = `${specifikasi[i]}`;
+      li.append(datali);
+      spec.appendChild(li);
+      close.addEventListener("click", () => {
+        li.parentNode.removeChild(li);
+        location.reload();
+      });
+    }
+
+    let newDesc = `<p class="text-light"> ${dbPcc[2].description}</p>`;
+    desc.insertAdjacentHTML("beforebegin", newDesc);
   }
 }
-
-const tutup = document.querySelector("#tutup");
-tutup.addEventListener("click", function () {
-  wraper.classList.toggle("open");
-});
---
+++ js/myjs.js
@@ -0,0 +1,42 @@
+const check = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+check.addEventListener("click", function () {
+  ul.classList.toggle("slide");
+});
+
+const text = ["Web Developer"];
+let count = 0;
+let textindex = 0;
+let curent = "";
+let word = "";
+(function ketik() {
+  if (count == text.length) {
+    count = 0;
+  }
+  curent = text[count];
+  word = curent.slice(0, ++textindex);
+  document.querySelector(".ketikan").textContent = word;
+  if (word.length == curent.length) {
+    count++;
+    textindex = 0;
+  }
+  setTimeout(ketik, 300);
+})();
+
+const titik = ["come", "..."];
+let coun = 0;
+// let b = 0;
+let indexA = 0;
+let first = "";
+let c = "";
+(function run() {
+  if ((b = titik.length)) {
+    coun = 0;
+  }
+
+  first = titik[coun];
+  c = first.slice(0, ++indexA);
+
+  setTimeout(run, 1000);
+})();
